54d09bebf3c015954694243a164c9915b885cdfa
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2017-06-24 11:50:18 -0700

    Add Google OAuth2 signin support

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
@@ -10,7 +11,19 @@ import (
 	"text/template"
 
 	"github.com/TheGhostHuCodes/GPB/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
 )
+
+type oauth2Provider struct {
+	ClientId     string
+	ClientSecret string
+}
+
+type oauth2Config struct {
+	SecurityKey string
+	Provider    map[string]oauth2Provider
+}
 
 // templ represents a single template
 type templateHandler struct {
@@ -32,10 +45,30 @@ func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the applicaiton.")
 	flag.Parse()
 
+	// Setup gomniauth
+	f, err := os.Open("oauth2_config.json")
+	if err != nil {
+		log.Fatalf("Could not open JSON config file.\nError: %s\n", err)
+	}
+	dec := json.NewDecoder(f)
+	var c oauth2Config
+	err = dec.Decode(&c)
+	f.Close()
+	if err != nil {
+		log.Fatalf("Could not decode JSON config file.\nError: %s\n", err)
+	}
+
+	gomniauth.SetSecurityKey(c.SecurityKey)
+	gomniauth.WithProviders(
+		google.New(c.Provider["Google"].ClientId, c.Provider["Google"].ClientSecret,
+			"http://localhost:8080/auth/callback/google"),
+	)
+
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// get the room going
 	go r.run()
